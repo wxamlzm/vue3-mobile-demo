8a6bdadb1315b6b6403dd92db9da3aeed608d737
wxamlzm <zd5024@163.com>
2025-04-28 14:17:31 +0800

    chore: 配置postcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import postPxToRem from 'postcss-pxtorem'
 
 // https://vite.dev/config/
 export default defineConfig(({}) => {
   return {
     plugins: [vue(), vueDevTools()],
-
+    css: {
+      postcss: {
+        plugins: [
+          postPxToRem({
+            rootValue: ({ file }) => {
+              return file?.includes('vant') ? 5 : 10
+            },
+            unitPrecision: 2,
+            propList: ['*'],
+            selectorBlackList: ['.ignore'],
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 2,
+            exclude: 'node_modules/(?!vant)',
+          }),
+        ],
+      },
+    },
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
