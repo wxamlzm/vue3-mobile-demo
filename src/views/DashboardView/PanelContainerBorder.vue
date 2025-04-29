1875297642fb76303f1d6f2b243260527ae71b1d
wxamlzm <zd5024@163.com>
2025-04-29 13:20:49 +0800

    feat: 添加svg组件

---
+++ src/views/DashboardView/PanelContainerBorder.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg
+    id=""
+    data-name=""
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    :width="713"
+    :height="1261.31"
+    viewBox="0 0 713 1261.31"
+  >
+    <defs>
+      <linearGradient
+        id="linear-gradient"
+        x1="14"
+        y1="2061.345"
+        x2="725"
+        y2="2061.345"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stop-color="#0cf"></stop>
+        <stop offset="0.195" stop-color="#0cf" stop-opacity="0"></stop>
+        <stop offset="0.797" stop-color="#0cf" stop-opacity="0"></stop>
+        <stop offset="1" stop-color="#0cf"></stop>
+      </linearGradient>
+    </defs>
+    <path
+      id=""
+      d="M34,1431.7H705l20,20V2671l-20,20H34l-20,-20V1451.7Z"
+      transform="translate(-13, -1430.69)"
+      stroke="url(#linear-gradient)"
+      fill-rule="evenodd"
+      :fill-opacity="0.1"
+    ></path>
+  </svg>
+</template>
